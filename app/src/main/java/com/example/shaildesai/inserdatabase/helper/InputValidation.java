c31662cf4eaa573f9659bbfba0c5c826387fbcb9
shail desai <shaildesai@shails-MacBook-Pro.local>
2018-10-12 16:08:51 -0300

    Created SQLite table in sqldatabse Helper class

--- app/src/main/java/com/example/shaildesai/inserdatabase/helper/InputValidation.java
+++ app/src/main/java/com/example/shaildesai/inserdatabase/helper/InputValidation.java
@@ -1,4 +1,19 @@
 package com.example.shaildesai.inserdatabase.helper;
 
+import android.content.Context;
+import android.support.design.widget.*;
+
+/**
+ * Created by Shail on Oct 12th 2018
+ */
+
 public class InputValidation {
+    private Context context;
+
+    public InputValidation(Context context){
+        this.context = context;
+    }
+
+
 }
+
